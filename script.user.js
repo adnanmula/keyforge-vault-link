9e81d973a08e5eec43c5e78b7fe81204e1d43746
adnan <33516302+adnanmula@users.noreply.github.com>
2023-08-05 23:50:30 +0200

    improve script

--- script.user.js
+++ script.user.js
@@ -1,35 +1,31 @@
 // ==UserScript==
 // @name         Keyforge Vault link to DoK
-// @author       adnanmula
 // @namespace    adnanmula
 // @version      0.1
 // @description  Adds a link to the dok website in the deck page of the kf vault
+// @author       You
 // @match        https://www.keyforgegame.com/deck-details/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=keyforgegame.com
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require      https://gist.githubusercontent.com/mjblay/18d34d861e981b7785e407c3b443b99b/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
 // @grant        none
 // ==/UserScript==
 
-waitForKeyElements(".deck-details__main-actions", doStuff);
+const url = document.querySelector('[property="og:url"]').getAttribute('content');
+const deckId = url.substring(42, url.lenght);
 
-function doStuff (node) {
-    const url = document.querySelector('[rel="canonical"]').getAttribute('href');
-    const deckId = url.substring(42, url.lenght);
+let container = document.querySelector('.page-header__navigation-links');
 
-    let container = document.querySelector('.deck-details__main-actions');
+let buttonWrapper = document.createElement('a');
+buttonWrapper.setAttribute('href', 'https://decksofkeyforge.com/decks/' + deckId);
+buttonWrapper.setAttribute('target', '__blank');
+buttonWrapper.setAttribute('style', 'margin-right: 10px');
 
-    let buttonWrapper = document.createElement('a');
-    buttonWrapper.setAttribute('href', 'https://decksofkeyforge.com/decks/' + deckId);
-    buttonWrapper.setAttribute('target', '__blank');
+let button = document.createElement('button');
+button.classList.add('btn-small-secondary');
 
-    let button = document.createElement('button');
-    button.classList.add('btn-medium-secondary');
+let buttonContent = document.createElement('span');
+buttonContent.innerHTML = 'DoK';
 
-    let buttonContent = document.createElement('span');
-    buttonContent.innerHTML = 'DoK';
-
-    button.appendChild(buttonContent);
-    buttonWrapper.appendChild(button);
-    container.appendChild(buttonWrapper);
-}
+button.appendChild(buttonContent);
+buttonWrapper.appendChild(button);
+container.prepend(buttonWrapper);
